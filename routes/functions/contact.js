e5599c258cf4bbd4c918bc9d4088f934cc45ab34
KnightShade <saurao2612@gmail.com>
2019-06-19 20:12:54 +0530

    mail can be sent

--- routes/functions/contact.js
+++ routes/functions/contact.js
@@ -1,4 +1,5 @@
-const sendmail = require('sendmail')();
+var nodemailer = require('nodemailer');
+const keys = require("../../config/keys");
 
 module.exports = (req, res) => {
     let name = req.body.name;
@@ -6,14 +7,26 @@ module.exports = (req, res) => {
     let mobile = req.body.mobile;
     let msg = req.body.msg;
 
-    sendmail({
-            from: email,
-            to: 'prashanthnayak920@gmail.com',
-            subject: 'test sendmail',
-            html: msg,
-        }, function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-        res.send("mail sent!");
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: keys.gmail.username,
+          pass: keys.gmail.password
+        }
+    });
+      
+    var mailOptions = {
+        from: keys.gmail.username,
+        to: 'saurao2612@gmail.com',
+        subject: 'Sending Email using Node.js',
+        text: name + ", " + email + ", " + mobile + ", " + msg
+    };
+      
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
     });
 }
