ca12bd6f534c3bfe4fd28bd0918fc9aa652069bc
KnightShade <saurao2612@gmail.com>
2019-06-20 19:55:18 +0530

    updation can be done

--- routes/functions/updateCourier.js
+++ routes/functions/updateCourier.js
@@ -2,16 +2,20 @@ var mysql = require('mysql');
 const con = require('../../db');
 
 module.exports = (req, res) => {
+    console.log(req.body);
     let trackId = req.body.trackId;
     let q = "select latitude, longitude from branch where branchid = " + req.user.branchid;
     con.query(q, (err, result) => {
-        q = "update couriers set latitude = " + result[0].latitude
-            + ", longitude = " + result[0].longitude
-            + " where trackId = " + trackId;
+        q = "update couriers set latitude = " + mysql.escape(result[0].latitude)
+            + ", longitude = " + mysql.escape(result[0].longitude)
+            + " where trackId = " + mysql.escape(trackId);
         console.log(q);
         con.query(q, (err, row, fields) => {
             if(err) res.send("Something went wrong! Try again please!");
-            res.send("Done!");
+            if(row.affectedRows)
+                res.send("Done!");
+            else
+                res.send("Invalid Package ID!");
         });
     });
 }
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -45,6 +45,6 @@ router.post("/contact", (req, res) => { contact(req, res); });
 
 router.get("/updateCourier", authCheck, (req, res) => { res.render("updateCourier"); });
 
-router.post("/update", (req, res) => { updateCourier(req, res); });
+router.post("/update", authCheck, (req, res) => { updateCourier(req, res); });
 
 module.exports = router;
